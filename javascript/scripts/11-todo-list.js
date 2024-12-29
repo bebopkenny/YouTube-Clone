ef8419ec6938a0e1fab7ed6c5bd0217bf93b20c7
kenny <kennygarcia15@csu.fullerton.edu>
2024-12-28 17:48:53 -0800

    Created a simple todo list

--- javascript/scripts/11-todo-list.js
+++ javascript/scripts/11-todo-list.js
@@ -1,10 +1,52 @@
-const todoList = [];
+const todoList = [{
+    name: '',
+    dueDate: '',
+}, {
+    name: '',
+    dueDate: '',
+}];
+
+renderTodoList();
+
+function renderTodoList() {
+    let todoListHTML = '';
+    for (let i = 0; i < todoList.length; i++) {
+        let todoObject = todoList[i]; // get the value/element
+        // const name = todoObject.name;
+        // const dueDate = todoObject.dueDate;
+        const { name, dueDate } = todoObject; // Object destructuring does the same thing
+        const html = `
+            <div>${name}</div> 
+            <div>${dueDate}</div>                
+            <button onclick="
+                todoList.splice(${i}, 1);
+                renderTodoList();
+            " class="delete-todo-button">Delete</button>
+            `;
+        todoListHTML += html; // to combine all html code
+    }
+    // console.log(todoListHTML);
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+}
+
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input'); // User's input in the place holder will get accessed here
     const name = inputElement.value; // Accessing the user's input
-    todoList.push(name);
-    console.log(todoList);
+    
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+    
+    todoList.push({
+        // name: name, 
+        // dueDate: dueDate,
+        name, // shorthand property: if the key & value have the same name you can just write it out once
+        dueDate,
+    });
+    // console.log(todoList);
 
     inputElement.value = ''; // Reset the input in the text box
+
+    renderTodoList();
 }
